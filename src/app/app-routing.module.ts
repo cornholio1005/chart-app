1b47dd5a32ee94bc360afb38cbac833bff72fa03
cornholio1005 <aranit.vokrri@gmail.com>
2022-06-03 18:30:49 +0200

    first chartApp commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,10 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
 ];
 
 @NgModule({
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit, AfterViewInit {
 
-  constructor() {}
+  @ViewChild('mychart') mychart: any;
+  canvas: any;
+  ctx: any;
 
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    this.canvas = this.mychart.nativeElement;
+    this.ctx = this.canvas.getContext('2d');
+
+    new Chart(this.ctx, {
+      type: 'bar',
+      data: {
+          datasets: [{
+              label: 'Current Vallue',
+              data: [0, 20, 40, 70, 77],
+              backgroundColor: "rgb(115 185 243 / 65%)",
+              borderColor: "#007ee7",
+              fill: true,
+          },
+          {
+            label: 'Invested Amount',
+            data: [0, 20, 40, 80],
+            backgroundColor: "#47a0e8",
+            borderColor: "#007ee7",
+            fill: true,
+        }],
+          labels: ['January 2019', 'February 2019', 'March 2019', 'April 2019', 'Mai']
+      },
+  });
+  }
 }
